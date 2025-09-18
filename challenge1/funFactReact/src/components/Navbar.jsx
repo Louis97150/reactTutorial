f926f2bcf3a88b650e0fcd7ca1adda7124dada77
Louis Duong <Loulouduong@gmail.com>
2025-09-18 21:00:08 +0200

    dossier challenge1 ajouté

---
+++ challenge1/funFactReact/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+  return(
+    <header>
+      <nav className="navBarre">
+        <img src="./images/react-logo.jpg" alt="React logo" id="reactLogo"/>
+        <span id="titreNavBarre">ReactFacts</span>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
