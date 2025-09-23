d81e7cd7b74ac92de74e3281a84346fc458c9a5a
Louis Duong <Loulouduong@gmail.com>
2025-09-23 09:21:50 +0200

    First step of the 2nd challenge

---
+++ challenge2/travelJournal/src/components/Entry.jsx
@@ -0,0 +1,26 @@
+function Entry(){
+    return(
+        <div id="fujiGlobal">
+            <img src="./images/mtFuji.jpg" alt="Mont Fuji" id="montagne"/>
+
+            <div id="location">
+                <div id="position">
+                    <img src="./images/marker.png" alt="Marker symbole" id="loca"/>
+                    <h3>JAPAN</h3>
+                    <a href="https://maps.app.goo.gl/mbco93iGbgwzxaxQ9">View on Google Maps</a>
+                </div>
+        
+                <h1>Mont Fuji</h1>
+                <h2>12 Jan, 2021 - 24 Jan, 2021</h2>
+                <p>
+                    Mount Fuji is the tallest mountain in Japan, standing
+                    at 3776 meter (12,380 feet). Mount Fuji is the single
+                    most popular tourist site in Japan, for both
+                    Japanese and foreign tourists
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Entry
---
+++ challenge2/travelJournal/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <header className="topBar">
+            <img src="./images/globe.png" alt="globe_image" id="globe"/>
+            <h1 id="titre">my travel journal.</h1>
+        </header>
+    )
+}
+
+export default Header
